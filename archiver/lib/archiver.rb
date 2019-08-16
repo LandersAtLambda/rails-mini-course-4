18e9017237d0255d18cd2b48e009df6f4c0a3a35
Mike Landers <landers.mike@gmail.com>
2019-08-15 21:31:57 -0700

    start of gem

---
+++ archiver/lib/archiver.rb
@@ -0,0 +1,15 @@
+require "archiver/version"
+
+module Archiver
+  class Error < StandardError; end
+  class Archiver
+    def archiver
+      
+    end
+
+    def archived?
+    end
+
+    def restore
+    end
+end
